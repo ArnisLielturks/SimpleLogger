170689ca48256a0a51da09212f3b48f76623faf8
arnislielturks <arnis.lielturks@accenture.com>
2015-06-01 09:21:37 +0000

    additional comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	//File handlers
+	//Create 3 output file stream buffers
 	ofstream coutFile;
 	coutFile.open("cout.log");
 	ofstream cerrFile;
@@ -18,16 +18,18 @@ int main(int argc, char* argv[])
 	streambuf* backupCerr = cerr.rdbuf();
 	streambuf* backupClog = clog.rdbuf();
 
-	//Replace cout,cerr,clog buffers
+	//Replace cout,cerr,clog buffers with our custom stream buffers
 	cout.rdbuf(coutFile.rdbuf());
 	cerr.rdbuf(cerrFile.rdbuf());
 	clog.rdbuf(clogFile.rdbuf());
 
+	//Write something into our files
 	cout << "This will be printed into cout.log file" << endl;
 	cerr << "This will be printed into cerr.log file" << endl;
 	clog << "This will be printed into clog.log file" << endl;
 
 	//Restore previous buffers
+	//NOTE!: It's important to do this, otherwise "segmentation fault" error will be generated
 	cout.rdbuf(backupCout);
 	cerr.rdbuf(backupCerr);
 	clog.rdbuf(backupClog);
